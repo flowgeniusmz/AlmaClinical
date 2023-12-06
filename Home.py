b5f0978cbaae353f357ea33514fd1df274cdcee9
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-06 11:40:41 -0600

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,7 +1,8 @@
 import streamlit as st
-from functions import login as lg, pagesetup as ps
+from functions import login as lg, pagesetup as ps, salesforce as sf
 from streamlit_modal import Modal
 import streamlit.components.v1 as components
+import pandas as pd
 
 
 #0. Page Config
@@ -11,5 +12,9 @@ st.set_page_config("AlmyAI", initial_sidebar_state="collapsed", layout="wide")
 if lg.check_authentication():
     ps.set_title("AlmyAI", "Clinical")
     ps.set_page_overview("Overview", "**AlmyAI - Clinical** is the first Alma AI-based app.")
-
+    dfClinical = sf.get_all_clinical_training()
+    dfClinical.to_csv("data/clinicaltraining.csv")
+    if "dfClinical" not in st.session_state:
+        st.session_state.dfClinical =[]
+    st.session_state.dfClinical = dfClinical
     
