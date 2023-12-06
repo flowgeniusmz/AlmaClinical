e72d84c644e49eb208210541f26390161477d96f
flowgeniusmz <128870971+flowgeniusmz@users.noreply.github.com>
2023-12-06 03:42:10 -0600

    Update salesforce.py

--- functions/salesforce.py
+++ functions/salesforce.py
@@ -18,25 +18,25 @@ def get_sfUserID(varEmail):
 
 def get_trainings(varUser):
   query = f"SELECT Account__c, Asset__c, Assigned_Email__c, Install_Date__c, Status__c FROM Clinical_Trainings__c WHERE Assigned_Email__c = '{varUser}'"  
-      data = sf.query(query)
-      records = data['records']
-  
-      # Create a list to store the data
-      training_data = []
-  
-      # Loop through each record
-      for record in records:
-          account = record['Account__c']
-          asset = record['Asset__c']
-          assigned_email = record['Assigned_Email__c']
-          install_date = record['Install_Date__c']
-          status = record['Status__c']
-  
-          # Add the data to the list
-          training_data.append([account, asset, assigned_email, install_date, status])
-  
-      # Create a DataFrame from the list
-      df = pd.DataFrame(training_data, columns=['Account', 'Asset', 'Assigned Email', 'Install Date', 'Status'])
-  
-      return df
+  data = sf.query(query)
+  records = data['records']
+
+  # Create a list to store the data
+  training_data = []
+
+  # Loop through each record
+  for record in records:
+      account = record['Account__c']
+      asset = record['Asset__c']
+      assigned_email = record['Assigned_Email__c']
+      install_date = record['Install_Date__c']
+      status = record['Status__c']
+
+      # Add the data to the list
+      training_data.append([account, asset, assigned_email, install_date, status])
+
+  # Create a DataFrame from the list
+  df = pd.DataFrame(training_data, columns=['Account', 'Asset', 'Assigned Email', 'Install Date', 'Status'])
+
+  return df
     
